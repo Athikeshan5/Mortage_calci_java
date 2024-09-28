8a797bd4d074bdc26162b5a7445b5aa8bbf39c3f
unknown <athikeshan5@gmail.com>
2024-09-28 15:32:44 +0530

    modify Mortage_Calculator

--- Mortage_Calculator.java
+++ Mortage_Calculator.java
@@ -6,13 +6,33 @@ public class Mortage_Calculator{
         int month=12;
         int percent=100;
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the principal amount");
-        int principal=sc.nextInt();
+        int principal=0;
+        float annual_interest_rate=0;
+        int period=0;
+        while(true){
+        System.out.println("enter the principal amount($1K-$1M)");
+        principal=sc.nextInt();
+        if(principal>=1000 && principal<=1000000){
+            break;
+        }
+        System.out.println("enter the principal amount between 1000 to 1000000");
+        }while(true){
         System.out.println("enter the annual interest rate");
-        float annual_interest_rate=sc.nextFloat();
+        annual_interest_rate=sc.nextFloat();
+        if (annual_interest_rate>0 && annual_interest_rate<=30){
+            break;
+        }
+        System.out.println("enter the annual interest rate between 1 to 30");
+        }
         float annual_percent=annual_interest_rate/month/percent;
+        while(true){
         System.out.println("enter the number of years");
-        int period=sc.nextInt();
+        period=sc.nextInt();
+        if(period>0 && period<=30){
+            break;
+        }
+        System.out.println("enter the period between 1 to 30");
+        }
         float period_month=period*month;
         double mortage=principal*(annual_percent*Math.pow(1+annual_percent,period_month)/(Math.pow(1+annual_percent,period_month)-1));
         System.out.println("$"+(float)mortage);
